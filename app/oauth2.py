3d98338ee0549dd52e6b0e7788bb96cc13b277dc
dAnkc0der <utkarshverma007@outlook.com>
2023-10-23 13:23:19 +0530

    made changes

--- app/oauth2.py
+++ app/oauth2.py
@@ -20,25 +20,18 @@ ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data: dict):
     to_encode = data.copy()
-
     expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp" : expire})
-
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, ALGORITHM)
-
     return encoded_jwt
 
 def verify_access_token(token: str, credentials_exception):
-
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-
         id: str = payload.get("user_id")
-
         if id is None:
             raise credentials_exception
         token_data = schemas.TokenData(id=id)
-
     except JWTError:
         raise credentials_exception
 
@@ -46,9 +39,6 @@ def verify_access_token(token: str, credentials_exception):
 
 def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
-
     token = verify_access_token(token, credentials_exception)
-
     user = db.query(models.User).filter(models.User.id == token.id).first()
-
     return user
